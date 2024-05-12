58a9dec67fbfb88991b355d54285a258f827ae16
andre-carbajal <carbajalvargasandre@gmail.com>
2024-05-12 11:32:03 -0500

    repositories created

---
+++ src/main/java/net/andrecarbajal/libraryapi/domain/author/AuthorRepository.java
@@ -0,0 +1,8 @@
+package net.andrecarbajal.libraryapi.domain.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
